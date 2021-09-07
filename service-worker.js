cf80abe8a01912a1472a962bba6363c6ab8843aa
Silver1987 <541075770@qq.com>
2021-09-07 13:00:12 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.2616c876df3e50b1f286614dca5c8f4f.js"
+  "/resume/precache-manifest.7af5fec5cc6ebbff989dcdd731d129cf.js"
 );
 
 self.addEventListener('message', (event) => {
